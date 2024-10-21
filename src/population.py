66dddb63b5c5d5fbbbee2cfbfc4fa4cd3330885b
Harsh Saindane <harshwardhansaindane@gmail.com>
2024-10-21 14:57:13 +0530

    Updated code

--- src/population.py
+++ src/population.py
@@ -1,23 +1,14 @@
 from schedule import Schedule
 
 class Population(object):
-  def __init__(self, size, data):
-    self.schedules = [ Schedule(data).initialize() for _ in range(size)]
+    def __init__(self, size, data):
+        self.schedules = [Schedule(data).initialize() for _ in range(size)]
 
-  def __str__(self):
-    return "".join([str(x) for x in self.schedules])
+    def __str__(self):
+        return "".join([str(x) for x in self.schedules])
 
-  def sort_by_fitness(self):
-    def _sort_schedule(schedule1, schedule2):
-      ret_val = 0
-      
-      if schedule1.fitness > schedule2.fitness:
-        ret_val = -1
-      elif schedule1.fitness < schedule2.fitness:
-        ret_val = 1
-      
-      return ret_val
-    
-    self.schedules = sorted(self.schedules, cmp=lambda x,y: _sort_schedule(x,y))
-    
-    return self
+    def sort_by_fitness(self):
+        # Sort schedules by fitness in descending order
+        self.schedules = sorted(self.schedules, key=lambda schedule: schedule.fitness, reverse=True)
+        
+        return self
--- src/utils.py
+++ src/utils.py
@@ -3,139 +3,84 @@ from random import random, seed
 from tabulate import tabulate
 
 def get_random_number():
-  seed()
-  return random()
+    seed()
+    return random()
 
 def print_msg(msg):
-  if isinstance(msg, dict):
-    msg = json.dumps(msg, indent=2)
-  
-  print
-  print "*" * 50
-  print msg
-  print "*" * 50
+    if isinstance(msg, dict):
+        msg = json.dumps(msg, indent=2)
+
+    print("\n" + "*" * 50)
+    print(msg)
+    print("*" * 50)
 
 def print_data(data):
-  print_msg("INPUT DATA INFORMATION")
-  print_msg("Available departments")
-  for dept in data.depts:
-    _courses = [str(x) for x in dept.courses]
-    print("name : %s, courses : %s" % (dept.name, _courses))
-  
-  print_msg("Available courses")
-  for course in data.courses:
-    _instructors = [str(x) for x in course.instructors]
-    _msg = [
-      "Course no.: %s, " % course.number,
-      "Name: %s" % course.name,
-      "Max no. of students: %s" % course.max_number_of_students,
-      "Instructors: %s" % _instructors
-    ]
-    print(",".join(_msg))
+    print_msg("INPUT DATA INFORMATION")
     
-  print_msg("Available rooms")
-  for room in data.rooms:
-    _msg = [
-      "Room No: %s" % room.number,
-      "Max seating capacity: %s" % room.seating_capacity
-    ]
-    print(",".join(_msg))
+    # Available departments
+    print_msg("Available departments")
+    for dept in data.depts:
+        courses = ', '.join(str(course) for course in dept.courses)
+        print(f"name: {dept.name}, courses: {courses}")
 
-  print_msg("Available instructors")
-  for instructor in data.instructors:
-    _msg = [
-      "ID: %s" % instructor.id,
-      "Name: %s" % instructor.name
-    ]
-    print(",".join(_msg))
-  
-  print_msg("Available meeting times")
-  for meeting_time in data.meeting_times:
-    _msg = [
-      "ID: %s" % meeting_time.id,
-      "Meeting Time: %s" % meeting_time.time
-    ]
-    print(",".join(_msg))
+    # Available courses
+    print_msg("Available courses")
+    for course in data.courses:
+        instructors = ', '.join(str(instr) for instr in course.instructors)
+        print(f"Course no.: {course.number}, Name: {course.name}, Max no. of students: {course.max_number_of_students}, Instructors: {instructors}")
+
+    # Available rooms
+    print_msg("Available rooms")
+    for room in data.rooms:
+        print(f"Room No: {room.number}, Max seating capacity: {room.seating_capacity}")
+
+    # Available instructors
+    print_msg("Available instructors")
+    for instructor in data.instructors:
+        print(f"ID: {instructor.id}, Name: {instructor.name}")
+
+    # Available meeting times
+    print_msg("Available meeting times")
+    for meeting_time in data.meeting_times:
+        print(f"ID: {meeting_time.id}, Meeting Time: {meeting_time.time}")
 
 def print_population_schedules(population, generation_number):
-  _schedule_number = 0
-  
-  print_msg("Generation Number: %s" % generation_number)
-  print
-  print
+    print_msg(f"Generation Number: {generation_number}")
 
-  _schedules = []
-  for x in population.schedules:
-    _schedules.append([
-      _schedule_number,
-      str(x),
-      x.fitness,
-      x.number_of_conflicts
-    ])
-    _schedule_number += 1
+    schedules = [
+        [
+            idx,
+            str(schedule),
+            schedule.fitness,
+            schedule.number_of_conflicts
+        ]
+        for idx, schedule in enumerate(population.schedules)
+    ]
 
-  headers = [
-    "Schedule #", 
-    "Classes [dept, class, room, instructor, meeting-time]", 
-    "Fitness", 
-    "Conflicts"
-  ]
-
-  print(tabulate(_schedules, headers=headers))
+    headers = ["Schedule #", "Classes [dept, class, room, instructor, meeting-time]", "Fitness", "Conflicts"]
+    print(tabulate(schedules, headers=headers))
 
 def print_schedule_as_table(data, schedule, generation):
-  _class_number = 1
-  
-  _classes = schedule.classes
-  _headers = [
-    "Class #",
-    "Dept",
-    "Course (number, max # of students)",
-    "Room (capacity)",
-    "Instructor (Id)",
-    "Meeting Time"
-  ]
+    table_data = []
+    headers = ["Class #", "Dept", "Course (number, max # of students)", "Room (capacity)", "Instructor (Id)", "Meeting Time"]
 
-  table_data = []
-  for _class in _classes:
-    major_idx = -1
-    for idx, _dept in enumerate(data.depts):
-      if _dept.name == _class.department.name:
-        major_idx = idx
+    for class_num, _class in enumerate(schedule.classes, start=1):
+        major_idx = next(idx for idx, dept in enumerate(data.depts) if dept.name == _class.department.name)
+        course_idx = next(idx for idx, course in enumerate(data.courses) if course.name == _class.course.name)
+        room_idx = next(idx for idx, room in enumerate(data.rooms) if room.number == _class.room.number)
+        instructor_idx = next(idx for idx, instructor in enumerate(data.instructors) if instructor.id == _class.instructor.id)
+        meeting_time_idx = next(idx for idx, meeting_time in enumerate(data.meeting_times) if meeting_time.id == _class.meeting_time.id)
 
-    course_idx = -1
-    for idx, _course in enumerate(data.courses):
-      if _course.name == _class.course.name:
-        course_idx = idx
-    
-    room_idx = -1
-    for idx, _room in enumerate(data.rooms):
-      if _room.number == _class.room.number:
-        room_idx = idx
-    
-    instructor_idx = -1
-    for idx, _instructor in enumerate(data.instructors):
-      if _instructor.id == _class.instructor.id:
-        instructor_idx = idx 
-    
-    meeting_time_idx = -1
-    for idx, _meeting_time in enumerate(data.meeting_times):
-      if _meeting_time.id == _class.meeting_time.id:
-        meeting_time_idx = idx
+        table_data.append([
+            class_num,
+            data.depts[major_idx].name,
+            f"{data.courses[course_idx].name} ({data.courses[course_idx].number}, {data.courses[course_idx].max_number_of_students})",
+            f"{data.rooms[room_idx].number} ({_class.room.seating_capacity})",
+            f"{data.instructors[instructor_idx].name} ({data.instructors[instructor_idx].id})",
+            f"{data.meeting_times[meeting_time_idx].time} ({data.meeting_times[meeting_time_idx].id})"
+        ])
 
-    table_data.append([
-      _class_number,
-      data.depts[major_idx].name,
-      "%s (%s, %s)" % (data.courses[course_idx].name, data.courses[course_idx].number, _class.course.max_number_of_students),
-      "%s (%s)" % (data.rooms[room_idx].number, _class.room.seating_capacity),
-      "%s (%s)" % (data.instructors[instructor_idx].name, data.instructors[instructor_idx].id),
-      "%s (%s)" % (data.meeting_times[meeting_time_idx].time, data.meeting_times[meeting_time_idx].id)
-    ])
-    _class_number += 1
+    print(tabulate(table_data, headers=headers))
 
-  print
-  print(tabulate(table_data, headers=_headers))
-  print
-
-  if schedule.fitness == 1.0:
-    print_msg("Solution Found in %s generations" % (generation + 1))
+    if schedule.fitness == 1.0:
+        print_msg(f"Solution Found in {generation + 1} generations")
